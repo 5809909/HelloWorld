9314f098cde5e7a8404a757251bcceb747fb3112
Viktar Krauchyk <5809909@tut.by>
2017-09-07 16:49:58 +0300

    Project Hello EPAM )2

--- app/src/main/java/com/example/victorkravchik/helloworld/MainActivity.java
+++ app/src/main/java/com/example/victorkravchik/helloworld/MainActivity.java
@@ -8,17 +8,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final TextView textView = (TextView) findViewById(R.id.textView);
         Button button1 = (Button)findViewById(R.id.button);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
                 textView.setText(R.string.hello);
                 Toast.makeText(getApplicationContext(), R.string.hello, Toast.LENGTH_SHORT).show();
             }
